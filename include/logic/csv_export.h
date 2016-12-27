31a2fb1e2eef548e63d2a3e87d3828f517e250c1
Lucas Hinderberger <git@lhinderberger.de>
2016-12-27 19:28:21 +0100

    Extended Documentation.

---
+++ include/logic/csv_export.h
@@ -0,0 +1,11 @@
+/* This file is part of prog1-beleg
+ * Written by Lucas Hinderberger, HTW Dresden, winter term of 2016/17
+ * See README for details
+ */
+
+#ifndef PB_CSV_EXPORT
+#define PB_CSV_EXPORT
+
+
+
+#endif
