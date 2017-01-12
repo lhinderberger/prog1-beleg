594529745bbf5a6c9ae02bba9ab9c655116521a0
Lucas Hinderberger <git@lhinderberger.de>
2017-01-12 11:20:07 +0100

    Stub: Begun working on Item Buffer

--- src/frontend/list.c
+++ src/frontend/list.c
@@ -1,18 +1,25 @@
 #include "frontend/globals.h"
 #include "frontend/error.h"
 #include "frontend/list.h"
+#include "logic/items_query.h"
 
 #include <glib/gi18n.h>
 
 GtkGrid * listGrid = NULL;
 GtkWidget * listScreen = NULL;
 GtkScrolledWindow * scrolledWindow = NULL;
-int n_items = 0;
+
+#define ITEM_BUF_SIZE 10
+pb_material_item * item_buf[ITEM_BUF_SIZE];
+int n_items_displayed = 0;
 
 void add_list_heading(int column, const char * label) {
+    /* Create label */
     GtkWidget * lbl_widget = gtk_label_new(label);
     if (!lbl_widget)
         fatal_error(widget_creation_error);
+
+    /* Add to grid */
     gtk_grid_attach(listGrid, lbl_widget, column, 0, 1, 1);
     gtk_widget_show(lbl_widget);
 }
@@ -27,7 +34,7 @@ void clear_list() {
     }
 
     /* Create a new one */
-    n_items = 0;
+    n_items_displayed = 0;
     listGrid = (GtkGrid*)gtk_grid_new();
     if (!listGrid)
         fatal_error(widget_creation_error);
@@ -43,14 +50,46 @@ void clear_list() {
 }
 
 void render_material_item(pb_material_item * item) {
-    GtkWidget * dummybutton = gtk_button_new();
+    g_assert(item);
+
+    /*GtkWidget * dummybutton = gtk_button_new();
     char buf[512];
     sprintf(buf, "Dummy Button %d", (int)item);
     gtk_button_set_label((GtkButton*)dummybutton, buf);
     gtk_grid_attach(listGrid, dummybutton, 0, 1 + n_items, 1, 1);
     gtk_widget_show(dummybutton);
-    n_items++;
+    n_items++;*/
     //TODO: Render actual material item
+
+    //TODO: Add image column
+
+    /* Add article name column */
+    GtkLabel * art_name_label = gtk_label_new(item->name);
+    if (!art_name_label)
+        fatal_error(widget_creation_error);
+    gtk_grid_attach(listGrid, art_name_label, 1, n_items_displayed, 1, 1);
+    gtk_widget_show(art_name_label);
+
+    /* Add article id column */
+    GtkLabel * art_id_label = gtk_label_new(item->article_no);
+    if (!art_id_label)
+        fatal_error(widget_creation_error);
+    gtk_grid_attach(listGrid, art_id_label, 2, n_items_displayed, 1, 1);
+    gtk_widget_show(art_id_label);
+
+    /* Add article id column */
+    char n_stock_str[32];
+    if (sprintf(n_stock_str, "%d", item->n_stock) < 0)
+        fatal_error("sprintf failed for n_stock");
+
+    GtkLabel * n_stock_label = gtk_label_new(n_stock_str);
+    if (!n_stock_label)
+        fatal_error(widget_creation_error);
+    gtk_grid_attach(listGrid, n_stock_label, 3, n_items_displayed, 1, 1);
+    gtk_widget_show(n_stock_label);
+
+    /* Finalize */
+    n_items_displayed++;
 }
 
 void show_material_list() {
@@ -68,6 +107,8 @@ void show_material_list() {
 
     //TODO: Load and render actual list
     clear_list();
-    for (int i = 0; i < 100; i++)
-        render_material_item((pb_material_item*)i);
+
+    int total_items = pb_list_mat_items(db, item_buf, PB_MAT_ITEM_VAR_NAME, 1, 0, ITEM_BUF_SIZE); //TODO: Check return value
+    for (int i = 0; i < total_items; i++)
+        render_material_item(item_buf[i]);
 }
